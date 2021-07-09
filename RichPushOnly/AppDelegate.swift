c30d40da1d11d23d1e7aafabca99cac480709c2b
Sunny <sl2883@cornell.edu>
2021-07-09 12:02:20 -0700

    CT basic

--- RichPushOnly/AppDelegate.swift
+++ RichPushOnly/AppDelegate.swift
@@ -6,13 +6,15 @@
 //
 
 import UIKit
-
+import CleverTapSDK
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        CleverTap.setDebugLevel(CleverTapLogLevel.debug.rawValue);
+        CleverTap.autoIntegrate();
         // Override point for customization after application launch.
         return true
     }

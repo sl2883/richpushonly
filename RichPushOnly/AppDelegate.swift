2e5eb47fd7e347c069800d8d24b3bb65ef05ee65
Sunny <sl2883@cornell.edu>
2021-07-09 12:48:53 -0700

    foreground pushes

--- RichPushOnly/AppDelegate.swift
+++ RichPushOnly/AppDelegate.swift
@@ -46,6 +46,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                    didReceive response: UNNotificationResponse,
+                                    withCompletionHandler completionHandler: @escaping () -> Void) {
+
+            NSLog("%@: did receive notification response: %@", self.description, response.notification.request.content.userInfo)
+            completionHandler()
+        }
+
+        func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                    willPresent notification: UNNotification,
+                                    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+
+            NSLog("%@: will present notification: %@", self.description, notification.request.content.userInfo)
+            CleverTap.sharedInstance()?.recordNotificationViewedEvent(withData: notification.request.content.userInfo)
+            completionHandler([.badge, .sound, .alert])
+        }
 
 
 }

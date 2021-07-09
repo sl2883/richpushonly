f5e9c1433c1205e63a9804955756fb7945ae51f9
Sunny <sl2883@cornell.edu>
2021-07-09 11:56:01 -0700

    Initial Commit

---
+++ RichPushOnly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RichPushOnly
+//
+//  Created by Sunny Ladkani on 7/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

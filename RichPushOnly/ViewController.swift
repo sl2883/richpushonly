dec66bf98c642c90cf9590b980edbe31fffc2703
Sunny <sl2883@cornell.edu>
2021-07-09 13:07:23 -0700

    adding triggers in frontend

--- RichPushOnly/ViewController.swift
+++ RichPushOnly/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CleverTapSDK
 
 class ViewController: UIViewController {
 
@@ -14,6 +15,45 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func login1(sender: UIButton) {
+        print("login1");
+        
+        let profile: Dictionary<String, Any> = [
+            //Update pre-defined profile properties
+            "Name": "Rich Push Only",
+            "Email": "richpushonly@testrichpushonly.com",
+            //Update custom profile properties
+            "Plan type": "Silver",
+            "Favorite Food": "Pizza"
+        ];
 
+        CleverTap.sharedInstance()?.onUserLogin(profile);
+    }
+    
+    @IBAction func pv(sender: UIButton) {
+        print("product viewed");
+        
+        let props: Dictionary<String, Any> = [
+            "Product name": "Casio Chronograph Watch",
+            "Category": "Mens Accessories",
+            "Price": 59.99,
+            "Date": NSDate()
+        ]
+
+        CleverTap.sharedInstance()?.recordEvent("Product viewed", withProps: props)
+    }
+    
+    @IBAction func pp(sender: UIButton) {
+        print("product purc");
+        
+        let props: Dictionary<String, Any> = [
+            "Product name": "Casio Chronograph Watch",
+            "Category": "Mens Accessories",
+            "Price": 59.99,
+            "Date": NSDate()
+        ]
+
+        CleverTap.sharedInstance()?.recordEvent("Product purchased", withProps: props)
+    }
 }
 
